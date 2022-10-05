f64a75c409567a3295f5cab1d67419d1570041e8
RAIAN-PC\raian <rycen618@gmail.com>
2022-10-04 19:02:20 -0700

    First shogi server work

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const app = express()
+
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on('connection', socket => {
+    console.log(socket.id)
+    socket.on('move', (selectedId, position, promote) => {
+        socket.broadcast
+    })
+})
